88b68a70f18f13aa3a5acd818ad66a52c093debf
@maiow <emmaria@yandex.ru>
2024-06-15 22:24:21 +0300

    add Slot-based layout, use paddingFromBaseline

--- MySoothe/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ MySoothe/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -179,12 +179,23 @@ fun FavoriteCollectionsGrid(
     }
 }
 
-// Step: Home section - Slot APIs
+// Slot APIs
 @Composable
 fun HomeSection(
-    modifier: Modifier = Modifier
+    @StringRes title: Int,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
 ) {
-    // Implement composable here
+    Column(modifier) {
+        Text(
+            text = stringResource(title),
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier
+                .paddingFromBaseline(top = 40.dp, bottom = 16.dp)
+                .padding(horizontal = 16.dp)
+        )
+        content()
+    }
 }
 
 // Step: Home screen - Scrolling
@@ -291,7 +302,11 @@ fun AlignYourBodyRowPreview() {
 @Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
 @Composable
 fun HomeSectionPreview() {
-    MySootheTheme { HomeSection() }
+    MySootheTheme {
+        HomeSection(R.string.align_your_body) {
+            AlignYourBodyRow()
+        }
+    }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
