c4ada9eb711c1b1c063f2e0ea73a4bdc8f701533
@maiow <emmaria@yandex.ru>
2024-07-06 11:41:08 +0300

    Initial

--- BasicState/app/src/main/java/com/mivanovskaya/basicstate/MainActivity.kt
+++ BasicState/app/src/main/java/com/mivanovskaya/basicstate/MainActivity.kt
@@ -3,11 +3,9 @@ package com.mivanovskaya.basicstate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,32 +14,27 @@ import com.mivanovskaya.basicstate.ui.theme.BasicStateTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             BasicStateTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    WellnessScreen()
                 }
             }
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+//}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BasicStateTheme {
-        Greeting("Android")
+        WellnessScreen(modifier = Modifier.fillMaxSize())
     }
 }
