b48d3958c927a859c33d2aa2d21b56aec8878939
@maiow <emmaria@yandex.ru>
2024-07-11 23:34:15 +0300

    some playing with state

--- BasicState/app/src/main/java/com/mivanovskaya/basicstate/WaterCounter.kt
+++ BasicState/app/src/main/java/com/mivanovskaya/basicstate/WaterCounter.kt
@@ -1,16 +1,26 @@
 package com.mivanovskaya.basicstate
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Text(
-        text = "You've had $count glasses.",
-        modifier = modifier.padding(16.dp)
-    )
+    Column(modifier = modifier.padding(16.dp)) {
+        // Changes to count are now tracked by Compose
+        var count by remember { mutableStateOf(0) }
+
+        Text("You've had ${count} glasses.")
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+            Text("Add one")
+        }
+    }
 }
