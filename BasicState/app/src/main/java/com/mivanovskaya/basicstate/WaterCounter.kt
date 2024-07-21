acc81d33f8e33fac8f85d18c5697a3107d29b4d4
@maiow <emmaria@yandex.ru>
2024-07-21 14:34:54 +0300

    add rememberSaveable, tasks list item

--- BasicState/app/src/main/java/com/mivanovskaya/basicstate/WaterCounter.kt
+++ BasicState/app/src/main/java/com/mivanovskaya/basicstate/WaterCounter.kt
@@ -7,8 +7,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -16,11 +15,11 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        // Changes to count are now tracked by Compose
-        var count by remember { mutableIntStateOf(0) }
-
-        Text("You've had $count glasses.")
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+        var count by rememberSaveable { mutableIntStateOf(0) }
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("Add one")
         }
     }
